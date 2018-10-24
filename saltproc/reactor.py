fbf32a87acc69402cd351908942d137429a40366
andrewryh <andrewryh@gmail.com>
2018-10-24 15:30:54 -0500

    Reactor class added

---
+++ saltproc/reactor.py
@@ -0,0 +1,32 @@
+class Reactor ():
+    """ Class contains information about current state of the reactor.
+    """
+
+    def __init__(
+            self,
+            name,
+            volume,
+            mass_flowrate,
+            power_level,
+            fp_powdens,
+            composition,
+            mthm):
+        """ Initializes the class
+        Parameters:
+        -----------
+        name: string
+            name of the reactor design
+        volume: float
+            total volume of reactor core (cm**3)
+        mass_flowrate: float
+            total mass flowrate through reactor (g/s)
+        power_level: array [Tx1]
+            normalized power level in percents (%) for each depletion step
+        fp_powdens: float
+            full power density (kW/g) in depletion simulation
+        composition: array [iso x T x number of depleted materials]
+            mass of each isotope for the end of each timestep for every fluid
+            (g)
+        mthm: array [Tx1]
+            metric tons of heavy metals for the end of each time step (MTHM)
+        """
