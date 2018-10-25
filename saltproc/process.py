ef7c7ffc57755b8fc7391f65aa8115cbe6604c9b
andrewryh <andrewryh@gmail.com>
2018-10-25 08:57:37 -0500

    class Process() finished

--- saltproc/process.py
+++ saltproc/process.py
@@ -6,15 +6,31 @@ class Process():
     def __init__(
                 self,
                 mass_flowrate,
+                capacity,
+                volume,
                 inflow,
                 outflow,
-                efficency,
-                volume):
+                efficency):
             """ Initializes the class
 
             Parameters:
             -----------
-            mat_name: string
-                name of material from SERPENT input file
+            mass_flowrate: float
+                mass flow rate of the material flow (g/s)
+            capacity: float
+                maximum mass flow rate of the material flow which current
+                 process can handle (g/s)
+            volume: float
+                total volume of the current facility (cm**3)
+            inflow: ndarray [ISO x number of inflows]
+                array with isotope vectors from other processes and/or reactor
+                 with mass moving into the current process (g)
+            outflow: ndarray [ISO x number of inflows]
+                array with isotope vectors from other processes and/or reactor
+                 with mass moving from the current procces (g)
+            efficency: ndarray [ISO x 1]
+                vector contains removal efficency vector for the current
+                 process (w.%)
+
 
             """
