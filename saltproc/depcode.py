7752164097d0701f6160e4e681147716fad6e3a0
andrewryh <andrewryh@gmail.com>
2018-11-01 15:22:15 -0500

    Serpent running function added

--- saltproc/depcode.py
+++ saltproc/depcode.py
@@ -1,30 +1,37 @@
-class Depcode():
+import subprocess
+
+
+class Depcode:
     """ Class contains information about input, output, geometry, and template
     file for running depletion simulation code. Also contains information about
-     neutrons population, active and inactive cycle, etc.
+     neutrons population, active and inactive cycle, etc. Read template and
+      output, write new input for the depletion code.
     """
 
     def __init__(
                 self,
                 codename,
-                template_filename,
-                input_filename,
-                output_filename,
-                npop,
-                active_cycles,
-                inactive_cycles):
+                exec_path,
+                template_fname,
+                input_fname,
+                output_fname,
+                npop=None,
+                active_cycles=None,
+                inactive_cycles=None):
             """ Initializes the class
 
             Parameters:
             -----------
             codename: string
                 name of the code for depletion
-            template_filename: string
+            exec_path: string
+                path to depletion code executable
+            template_fname: string
                 name of user input file for depletion code with geometry and
                  initial composition
-            input_filename: string
+            input_fname: string
                 name of input file for depletion code rerunning
-            output_filename: string
+            output_fname: string
                 name of output file for depletion code rerunning
             npop: int
                 size of neutron population for Monte-Carlo code
@@ -33,3 +40,46 @@ class Depcode():
             inactive_cycles: int
                 number of inactive cycles
             """
+            # initialize all object attributes
+            self.codename = codename
+            self.exec_path = exec_path
+            self.template_fname = template_fname
+            self.input_fname = input_fname
+            self.output_fname = output_fname
+            self.npop = npop
+            self.active_cycles = active_cycles
+            self.inactive_cycles = inactive_cycles
+
+    def run_depcode(self, cores):
+        """ Runs depletion code as subprocess with the given parameters"""
+        print('Running %s' % self.codename)
+        args = (self.exec_path, '-omp', str(cores), self.input_fname)
+        try:
+            subprocess.check_call(args)
+        except subprocess.CalledProcessError as error:
+            print(error.output)
+            raise ValueError('\n %s RUN FAILED\n' % self.codename)
+        print('Finished Serpent Run')
+
+    def read_depcode_template(self):
+        """ Reads prepared template (input) file for Depeletion code for
+         further changes in the file to prepare input file for multiple runs"""
+        file = open(self.template_fname, 'r')
+        str_list = file.readlines()
+        if self.npop and self.active_cycles and self.inactive_cycles:
+            sim_param = [s for s in str_list if s.startswith("set pop")]
+            if len(sim_param) > 1:
+                print('ERROR: Template file %s contains multiple lines with '
+                      'simulation parameters:\n'
+                      % (self.template_fname), sim_param)
+                return
+            elif len(sim_param) < 1:
+                print('ERROR: Template file %s does not contain line with '
+                      'simulation parameters.' % (self.template_fname))
+                return
+            args = 'set pop %i %i %i\n' % (self.npop, self.active_cycles,
+                                           self.inactive_cycles)
+            new_str_list = [s.replace(sim_param[0], args) for s in str_list]
+            out_file = open('test.txt', 'w')
+            out_file.writelines(new_str_list)
+            out_file.close()
---
+++ saltproc/test.py
@@ -0,0 +1,36 @@
+import os
+from depcode import Depcode
+
+input_path = os.path.dirname(os.path.abspath(__file__))
+print(input_path)
+input_file = os.path.join(input_path, 'data/rebus.inp')
+template_file = os.path.join(input_path, 'data/rebus.inp')
+# executable path of Serpent
+exec_path = '/home/andrei2/serpent/serpent2/src_2130/sss2'
+# Number of cores and nodes to use in cluster
+cores = 4
+
+if __name__ == "__main__":
+    print('Initiating Saltproc:\n'
+          # '\tRestart = ' + str(restart) + '\n'
+          # '\tNodes = ' + str(nodes) + '\n'
+          '\tCores = ' + str(cores) + '\n'
+          # '\tSteps = ' + str(steps) + '\n'
+          # '\tBlue Waters = ' + str(bw) + '\n'
+          '\tSerpent Path = ' + exec_path + '\n'
+          '\tInput File Path = ' + input_file + '\n'
+          # '\tMaterial File Path = ' + mat_file + '\n'
+          # '\tOutput DB File Path = ' + db_file + '\n'
+          )
+
+    run = Depcode(codename='SERPENT',
+                  exec_path=exec_path,
+                  template_fname=template_file,
+                  input_fname=input_file,
+                  output_fname='NONE',
+                  npop=444,
+                  active_cycles=100,
+                  inactive_cycles=20)
+#    run.run_depcode(cores)
+# print('End of RUN')
+    run.read_depcode_template()
