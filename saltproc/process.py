93d8ac054192977def9ad40d8b97f180031295ba
andrewryh <andrewryh@gmail.com>
2020-01-23 11:31:49 -0600

    Clean out extra code from `Process`

--- saltproc/process.py
+++ saltproc/process.py
@@ -6,66 +6,59 @@ import gc
 
 
 class Process():
-    """ Class describes process which must be applied to Materialflow to change
-     composition, direction, etc.
+    """Class describes process which must be applied to Materialflow to change
+     burnable material composition.
      """
 
     def __init__(self,
-                 mass_flowrate,
-                 capacity,
-                 volume,
-                 # inflow,
-                 # outflow,
-                 # waste_stream_name,
-                 efficiency,
-                 # rem_iso
+                 mass_flowrate=0.0,
+                 capacity=0.0,
+                 volume=0.0,
+                 efficiency=1.0,
                  ):
-        """ Initializes the class
+        """ Initializes the Process object.
 
-        Parameters:
-        -----------
-        mass_flowrate: float
+        Parameters
+        ----------
+        mass_flowrate : float
             mass flow rate of the material flow (g/s)
-        capacity: float
-            maximum mass flow rate of the material flow which current
-             process can handle (g/s)
-        volume: float
-            total volume of the current facility (cm**3)
-        inflow: Materialflow object
-            array with isotope vectors from other processes and/or reactor
-             with mass moving into the current process (g)
-        outflow: Materialflow object
-            array with isotope vectors from other processes and/or reactor
-             with mass moving from the current procces back to the core (g)/s?
-        waste_stream: Materialflow object
-            array with isotope vectors from other processes and/or reactor
-             with mass moving from the current procces to waste stream (g)/s?
-        efficiency: dict
-                    key: element name for removal (not isotope)
-                    value: removal efficency for the isotope (weight fraction)
+        capacity : float
+            maximum mass flow rate of the material flow which current process
+            can handle (g/s)
+        volume : float
+            total volume of the current facility (:math:`cm^3`)
+        efficiency : dict
+
+            ``key``
+                element name for removal (not isotope)
+            ``value``
+                removal efficency for the isotope (weight fraction)
         """
         # initialize all object attributes
         self.mass_flowrate = mass_flowrate
         self.capacity = capacity
         self.volume = volume
-        # self.inflow = inflow
-        # self.outflow = outflow
-        # self.waste_stream_name = waste_stream_name
         self.efficiency = efficiency
 
-    # @profile
     def rem_elements(self, inflow):
-        """ Returns PyNE material after removal target isotopes from inflow
-         with specified efficiency and waste stream PyNE material
+        """Updates Materialflow object `inflow` after removal target isotopes
+        with specific efficiency in single component of fuel reprocessing
+        system and returns waste stream Materialflow object.
+
+        Parameters
+        ----------
+        inflow : Materialflow obj
+            Target material stream to remove poisons from.
+
+        Returns
+        -------
+        Materialflow object
+            Waste stream from the reprocessing system component.
+
         """
-        # print("Flow Before reprocessing %f g" % inflow.mass)
-        # inflow.mass = inflow.mass*(self.mass_flowrate/9.92E+6)
-        # inflow.norm_comp()
+
         waste_nucvec = {}
         out_nucvec = {}
-        # mass flow fraction via the processes
-        # m_frac = float(self.mass_flowrate/9.92e+6)
-        # print("Flow Before reprocessing % g" % inflow.mass)
         print("Xe concentration in inflow before % f g" % inflow['Xe136'])
         for iso in inflow.comp.keys():
             el_name = pyname.serpent(iso).split('-')[0]
@@ -75,8 +68,6 @@ class Process():
                     float(1.0 - self.efficiency[el_name])
                 waste_nucvec[iso] = \
                     float(inflow[iso]) * float(self.efficiency[el_name])
-                # print(el_name, iso, inflow[iso], waste_nucvec[iso],
-                #       self.efficiency[el_name])
             else:
                 out_nucvec[iso] = float(inflow.comp[iso])
                 waste_nucvec[iso] = 0.0  # zeroes everywhere else
@@ -84,19 +75,13 @@ class Process():
         inflow.mass = float(inflow.mass - waste.mass)
         inflow.comp = out_nucvec
         inflow.norm_comp()
-        # Recalculate volume too (assumed constant density)
-        # inflow.vol = float(inflow.mass * inflow.density)
-        # outflow.copy_pymat_attrs(inflow)  # Copy additional PyNE attributes
-        # print(waste.print_attr())
         """print("Waste class %s and mass %f g " % (waste.__class__, waste.mass))
         print("Outflow class ", outflow.__class__)
         print("Mass ", outflow.mass, inflow.mass, outflow.mass - inflow.mass)
         print("Density ", outflow.density, inflow.density)
-        print("atoms_per_molecule ", outflow.atoms_per_molecule == inflow.atoms_per_molecule)
         print("Volume  ", outflow.vol == inflow.vol)
         print("Burnup ", outflow.burnup == inflow.burnup)
         print("Metadata ", outflow.metadata, inflow.metadata)"""
-        # print("Flow After reprocessing ^^^^\n\n", inflow.print_attr())
         print("Xe concentration in inflow after %f g" % inflow['Xe136'])
         print("Waste mass %f g\n" % waste.mass)
         del out_nucvec, waste_nucvec, el_name
