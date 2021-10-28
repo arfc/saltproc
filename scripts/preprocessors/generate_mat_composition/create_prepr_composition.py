f40b6eff5f874d23d685599b1e227a91cce20f86
yardasol <yardasol@grinnell.edu>
2021-10-28 16:20:43 -0500

    update script

--- scripts/preprocessors/generate_mat_composition/create_prepr_composition.py
+++ scripts/preprocessors/generate_mat_composition/create_prepr_composition.py
@@ -1,13 +1,12 @@
-from saltproc import Depcode
+from saltproc import DepcodeSerpent
 # serpent_dep_m_out = './tap.serpent'
 serpent_dep_m_out = '../../examples/msbr/msbr.serpent'
 new_mat_file = './msbr_saltproc_prepr_comp.ini'
 
-depcode = Depcode(codename='SERPENT',
-                  exec_path=None,
-                  template_fname=None,
-                  input_fname=serpent_dep_m_out,
-                  iter_matfile=None,
-                  geo_file=None)
+depcode = DepcodeSerpent(exec_path=None,
+                         template_fname=None,
+                         input_fname=serpent_dep_m_out,
+                         iter_matfile=None,
+                         geo_file=None)
 mats = depcode.read_dep_comp(serpent_dep_m_out, 0)
 depcode.write_mat_file(mats, new_mat_file, 0)
