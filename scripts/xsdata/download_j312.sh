f05c2b4e26a0cb42c32eeb79f84d5e7f266f857d
yardasol <yardasol@grinnell.edu>
2021-12-11 18:29:02 -0600

    add line to allow users to specify where libary gets downloaded

--- scripts/xsdata/download_j312.sh
+++ scripts/xsdata/download_j312.sh
@@ -1,19 +1,18 @@
+#DATADIR is the directory where the xs library is extracted to
+DATADIR=$(pwd)
 LN="https://www.oecd-nea.org/dbforms/data/eva/evatapes/jeff_31/JEFF312/ACE/"
 SLUG="ACEs_"
 EXT="K.zip"
 SLUG1="STL_ACEs.zip"
-ALL=(500 600 800 900 1000 1200 1500)
-TEST=(900)
-
-TEMPS=$TEST
+TEMPS=(900)
 
 #Uncomment if you want all temperatures (will take a long time)
-#TEMPS=$ALL
+#TEMPS=(500 600 800 900 1000 1200 1500)
 
 for T in ${TEMPS[@]}
 do
-    wget $LN$SLUG$T$EXT
+    wget -P $DATADIR $LN$SLUG$T$EXT
 done
 
 # Get thermal XS
-wget -O ACEs_THERMK.zip $LN$SLUG1
+wget -O ACEs_THERMK.zip -P $DATADIR $LN$SLUG1
