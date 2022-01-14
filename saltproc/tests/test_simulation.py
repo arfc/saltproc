639cf2298ca40b6236bf077995d9eef8ec6d1c08
yardasol <yardasol@grinnell.edu>
2022-01-14 16:38:45 -0600

    typo fixes

--- saltproc/tests/test_simulation.py
+++ saltproc/tests/test_simulation.py
@@ -35,15 +35,17 @@ def test_check_switch_geo_trigger():
     consistent with its docstring.
     """
 
-   switch_times = [1.0, 3, -31, 86.23333, 1e-16, 2e-18, "two o clock"]
-   assert simulation.check_switch_geo_trigger(1.0, switch_times) is True
-   assert simulation.check_switch_geo_trigger(3, switch_times) is True
-   assert simulation.check_switch_geo_trigger(-32, switch_times) is False
-   assert simulation.check_switch_geo_trigger(86.233, switch_times) is False
-   assert simulation.check_switch_geo_trigger(1e-16, switch_times) is True
-   assert simulation.check_switch_geo_trigger(5e-18, switch_times) is False
-   assert simulation.check_switch_geo_trigger("three o clock") is False
-   assert simulation.check_switch_geo_trigger("two o clock") is True
+    switch_times = [1.0, 3, -31, 86.23333, 1e-16, 2e-18, "two o clock"]
+    assert simulation.check_switch_geo_trigger(1.0, switch_times) is True
+    assert simulation.check_switch_geo_trigger(3, switch_times) is True
+    assert simulation.check_switch_geo_trigger(-32, switch_times) is False
+    assert simulation.check_switch_geo_trigger(86.233, switch_times) is False
+    assert simulation.check_switch_geo_trigger(1e-16, switch_times) is True
+    assert simulation.check_switch_geo_trigger(5e-18, switch_times) is False
+    assert simulation.check_switch_geo_trigger("three o clock",
+                                               switch_times) is False
+    assert simulation.check_switch_geo_trigger("two o clock",
+                                               switch_times) is True
 
 #def test_store_after_repr():
 
