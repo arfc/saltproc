c18e393d3aaa01228abba18e66b2f26a19a2a23b
yardasol <yardasol@grinnell.edu>
2022-01-19 15:20:26 -0600

    make versions consistent

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,7 @@ author = u'Andrei Rykhlevskii, Jin Whan Bae, Kathryn D. Huff'
 # The short X.Y version
 version = '0.3'
 # The full version, including alpha/beta/rc tags
-release = '0.3.0-dev'
+release = '0.3.1-dev'
 
 
 # -- General configuration ---------------------------------------------------
