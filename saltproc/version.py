d738e32b52605e62301ab9f47ba969e743924afe
yardasol <yardasol@grinnell.edu>
2022-01-30 11:40:11 -0600

    inital version of workflow to populate releasenotes

--- saltproc/version.py
+++ saltproc/version.py
@@ -3,8 +3,8 @@ from os.path import join as pjoin
 
 # Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
 _version_major = 0
-_version_minor = 3
-_version_micro = '1'  # use '' for first of series, number for 1 and above
+_version_minor = 4
+_version_micro = '0'  # use '' for first of series, number for 1 and above
 _version_extra = 'dev'
 # _version_extra = '0'  # Uncomment this for full releases
 
