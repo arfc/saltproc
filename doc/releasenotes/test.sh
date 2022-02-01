6a6308b2de95356e88cb9250fa3bca37313454ad
yardasol <yardasol@grinnell.edu>
2022-01-31 18:06:13 -0600

    officially remove cruft

--- doc/releasenotes/test.sh
+++
@@ -1,8 +0,0 @@
-files=$(git diff --name-only HEAD HEAD~1)
-
-#I'm trying to make a script that will get the version nams
-# from the changelogs to pass ot a gh action
-for file in $files
-do
-    IFS='/' read -ra arr <<< "$file"
-    VERSION=lnlnlnlf
