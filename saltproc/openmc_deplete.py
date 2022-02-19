d4e525bc8a5b8c71d48e13c21d2eecdd3223344d
yardasol <yardasol@grinnell.edu>
2022-02-19 12:24:59 -0600

    finish machinery to read in depletion_settings in openmc_deplete.py

--- saltproc/openmc_deplete.py
+++ saltproc/openmc_deplete.py
@@ -7,9 +7,6 @@ def parse_arguments():
 
     Parameters
     ----------
-
-    Returns
-    -------
     materials: str
         Path to openmc material `.xml` file
     geometry: int
@@ -21,41 +18,45 @@ def parse_arguments():
 
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('-mat',      # Number of nodes to use
-                        type=int,
+    parser.add_argument('-mat',
+                        type=str,
                         default=1,
                         help='path to openmc material \
                         material xml file')
-    parser.add_argument('-geo',      # Number of cores to use
-                        type=int,
+    parser.add_argument('-geo',
+                        type=str,
                         default=1,
                         help='path to openmc geometry \
                         xml file')
-    parser.add_argument('-set',      # main input file
+    parser.add_argument('-set',
                         type=str,
                         default=None,
                         help='path to openmc settings \
                         xml file')
+    parser.add_argument('-dep',
+                        type=str,
+                        default=None,
+                        help='path to depletion settings')
     args = parser.parse_args()
-    return int(args.mat), int(args.geo), str(args.set)
+    return str(args.mat), str(args.geo), str(args.set), str(args.dep)
 
+args = parse_arguments()
 
 # Initalize OpenMC objects
 materials = openmc.Materials.from_xml(path=args.mat)
 geometry = openmc.Geometry.from_xml(path=args.geo, materials=materials)
 settings = openmc.Settings.from_xml(args.set)
 tallies = openmc.Tallies.from_xml('./saltproc_tallies.xml')
-
-## NEED TO IMPLEMENT FUNCTION TO MAKE THIS XML ##
-## IN DepcodeOpenMC ##
-depletion_settings = # read xml object from file?
-
 model = openmc.model.Model(materials=materials,
                            geometry=geometry,
                            settings=settings
                            tallies=tallies)
 
+depletion_settings = {}
+with open(args.dep) as f:
+    depletion_settings = json.load(f)
+
 model.deplete(depletion_settings['timesteps'],
-              directory=depletion_settings['directory']
+              directory=depletion_settings['directory'],
               operator_kwargs=depletion_settings['operator_kwargs'],
               depletion_settings['integrator_kwargs'])
