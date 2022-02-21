7d9b715072b14bd3154838aef1795d9fbb76a821
yardasol <yardasol@grinnell.edu>
2022-02-21 14:08:29 -0600

    start script for converting serpent geometry to openmc geometry

---
+++ scripts/preprocessors/generate_geometry/convert_geometry_serpent_to_openmc.py
@@ -0,0 +1,23 @@
+import re
+import os
+import sys
+from pyne import nucname as pyname
+import openmc
+
+geo_dict = {
+    "surf": {
+        "inf": "",
+        "px": "",
+        "py": "",
+        "pz": "",
+        "plane": "",
+        "cylx": "",
+        "cyly":, "",
+        "cylz": "",
+        "cyl": "",
+        "cylv": "",
+        "sph": "",
+        "cone": "",
+        ""
+    }
+}
