80e000f2ea9420d19e785d88380a1bf511673499
yardasol <yardasol@grinnell.edu>
2022-02-22 15:58:56 -0600

    fleshed out cell creation

--- scripts/preprocessors/generate_geometry/convert_geometry_serpent_to_openmc.py
+++ scripts/preprocessors/generate_geometry/convert_geometry_serpent_to_openmc.py
@@ -30,6 +30,13 @@ geo_dict = {
         "3": openmc.HexLattice ## ADD SUPPORT FOR MORE LATTICE TYPES
     }
 }
+# based on openmc.Region.from_expression()
+region_operator_dict = {
+    " ": " ", # intersection
+    "-": "-", # complement
+    "#": "~", # cell complement? unsuppored
+    ":": "|", # union
+}
 
 param_index_dict = {
     "surf": {
@@ -89,7 +96,7 @@ CELL_REGEX1 = COMMENT_IGNORE_BEG_REGEX + \
     "cell(\s+[a-zA-Z0-9]+){4,}" + \
     COMMENT_IGNORE_END_REGEX
 CELL_REGEX2 = COMMENT_IGNORE_BEG_REGEX + \
-    "cell(\s+[a-zA-Z0-9]+){2}\s+fill(\s+[a-zA-Z0-9]+){2,}" + \
+    "cell(\s+[a-zA-Z0-9]+){2}\s+fill(\s+\-*\:*\#*[a-zA-Z0-9]+)+" + \
     COMMENT_IGNORE_END_REGEX
 CELL_REGEX3 = COMMENT_IGNORE_BEG_REGEX + \
     "cell(\s+[a-zA-Z0-9]+){2}\s+outside(\s+[a-zA-Z0-9]+)+" + \
@@ -102,9 +109,71 @@ universe_to_cell_dict = {}
 universe_dict = {}
 
 openmc_geometry = openmc.Geometry([])
+
+
+def _construct_region_helper(cell_line, cell_type):
+    """Creates input for use in openmc.Region.from_expression
+
+    Parameters
+    ----------
+    cell_line : str
+        A string representing a serpent `cell` card
+    cell_type :
+        A string representing the type of cell. Can be
+        `'material'`, `'fill'`, or `'outside'`
+
+    Returns
+    -------
+    my_expression : str
+        Boolean expression relating surface half-spaces. Should match
+        `expression` in openmc.Region.from_expression
+    my_surf_dict : dict of int to openmc.Surface
+        Dictionary relating surface IDs to openmc.Surface objects. Should
+        match `surfaces` in openmc.Region.from_expression
+    """
+    # Currently only implementing 'fill' type cell
+    if cell_type == 'material':
+        my_line = re.search(CELL_REGEX1, cell_line).group(0)
+        cell_line_splitter = "cell(\s+[a-zA-Z0-9]+){3}\s+"
+    elif cell_type == 'fill':
+        my_line = re.search(CELL_REGEX2, cell_line).group(0)
+        cell_line_splitter = cell_type
+    else:
+        raise ValueError("Incorrect cell type")
+
+    line_data = my_line.split()
+    my_surf_dict = {}
+    surf_name_to_surf_id = {}
+    my_region_surfaces = line_data[4:] # all cell types surface data is in the same place
+    # construct my_surf_dict
+    for s in my_region_surfaces:
+        surf_name = s.strip(" ")
+        surf_name = surf_name.strip("-")
+        surf_name = surf_name.strip(":")
+        openmc_surf = surf_dict[surf_name]
+        surf_id = openmc_surf.id
+        my_surf_dict[surf_id] = openmc_surf
+        surf_name_to_surf_id[openmc_surf.name] = str(surf_id)
+
+    #construct my_expression
+    if cell_type == 'material':
+        my_expression = re.search(cell_line_splitter,my_line) # match object
+        split_index = my_expression.span()[-1]
+        my_expression = my_line[split_index:]
+    else:
+        my_expression = my_line.split(cell_line_splitter)[-1]
+    # replace operators
+    my_expression = my_expression.replace(":", "|")
+    my_expression = my_expression.replace("#", "~")
+    for surf_name in surf_name_to_surf_id:
+        my_expression = my_expression.replace(surf_name,
+                                              surf_name_to_surf_id[surf_name])
+    return my_expression, my_surf_dict
+
 for line in geo_data:
-   line_data = line.split()
+    # Create openmc Surface objects
     if re.search(SURF_REGEX, line):
+        line_data = line.split()
         my_openmc_surf = geo_dict['surf'][line_data[2]].copy()
         ## addflow control for special cases
         # inf
@@ -120,13 +189,19 @@ for line in geo_data:
         my_openmc_surf.name = line_data[1]
         surf_dict[line_data[1]] = my_openmc_surf
 
+    # Create openmc Cell objects
     elif re.search(CELL_REGEX1, line):
-        my_region = ...
+        line_data = line.split()
+        cell_type = "material"
+        my_expression, my_region_surfaces = _construct_region_helper(line, cell_type)
+        my_region = openmc.Region.from_expression(my_expression, my_region_surfaces)
+
         my_cell_args = {
             "name": line_data[1],
             "fill": mat_dict[line_data[3]],
-            "region":
+            "region": my_region
         }
+
         my_openmc_cell = openmc.Cell(**my_cell_args)
         cell_dict[line_data[1]] = my_openmc_cell
         if bool(universe_to_cell_dict.get(line_data[2])):
@@ -134,7 +209,29 @@ for line in geo_data:
         else:
             universe_to_cell_dict[line_data[2]] = { line_data[1]: my_openmc_cell }
 
+
+
+    # fill cell
     elif re.search(CELL_REGEX2, line):
+        line_data = line.split()
+        cell_type = "fill"
+        my_expression, my_region_surfaces = _construct_region_helper(line, cell_type)
+        my_region = openmc.Region.from_expression(my_expression, my_region_surfaces)
+
+        my_cell_args = {
+            "name": line_data[1],
+            "fill": mat_dict[line_data[3]],
+            "region": my_region
+        }
+
+        my_openmc_cell = openmc.Cell(**my_cell_args)
+        cell_dict[line_data[1]] = my_openmc_cell
+        if bool(universe_to_cell_dict.get(line_data[2])):
+            universe_to_cell_dict[line_data[2]][line_data[1]] = my_openmc_cell
+        else:
+            universe_to_cell_dict[line_data[2]] = { line_data[1]: my_openmc_cell }
+
+
         my_cell_args = {
             "name": line_data[1],
             "fill":
