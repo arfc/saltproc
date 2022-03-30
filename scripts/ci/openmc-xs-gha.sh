7f6f774059936e2feae5a4ac60ed8ac479e4d7d4
yardasol <oyardas2@illinois.edu>
2022-03-30 12:31:27 -0500

    try calling install script from within another script

---
+++ scripts/ci/openmc-xs-gha.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+set -ex
+
+# try to run from source
+source scripts/ci/openmc-xs.sh
+
--- scripts/ci/openmc-xs.sh
+++ scripts/ci/openmc-xs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ex
 
-wget -O - https://anl.box.com/shared/static/9igk353zpy8fn9ttvtrqgzvw1vtejoz6.xz | tar -C $HOME -xJ
+wget -q -O - https://anl.box.com/shared/static/9igk353zpy8fn9ttvtrqgzvw1vtejoz6.xz | tar -C $HOME -xJ
 
