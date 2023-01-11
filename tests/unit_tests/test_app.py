398cb13d36a6fc2e3d7c6bc34987f86fb964083a
yardasol <oyardas2@illinois.edu>
2023-01-11 13:53:15 -0600

    Add tests that check various cases for OpenMC depletion input

--- tests/unit_tests/test_app.py
+++ tests/unit_tests/test_app.py
@@ -6,6 +6,21 @@ import pytest
 from saltproc.app import read_main_input, get_extraction_processes
 from saltproc.app import get_feeds, get_extraction_process_paths
 
+expected_depletion_settings = {'method': 'predictor',
+                                   'final_step': True,
+                                   'operator_kwargs': {
+                                       'diff_burnable_mats': False,
+                                       'normalization_mode': 'fission-q',
+                                       'fission_q': None,
+                                       'dilute_initial': 1000,
+                                       'fission_yield_mode': 'constant',
+                                       'fission_yield_opts': None,
+                                       'reaction_rate_mode': 'direct',
+                                       'reaction_rate_opts': None,
+                                       'reduce_chain': False,
+                                       'reduce_chain_level': None},
+                                   'output': True,
+                                   'integrator_kwargs': {}}
 
 @pytest.mark.parametrize("codename, ext", [
     ("serpent", ".ini"),
@@ -30,18 +45,7 @@ def test_read_main_input(cwd, codename, ext):
         assert depcode_input['chain_file_path'] == \
             str((input_path / 'test_chain.xml').resolve())
         assert depcode_input['depletion_settings'] == \
-            {'method': 'predictor',
-             'final_step': True,
-             'operator_kwargs': {'normalization_mode': 'fission-q',
-                                 'fission_q': None,
-                                 'dilute_initial': 1000,
-                                 'fission_yield_mode': 'constant',
-                                 'reaction_rate_mode': 'direct',
-                                 'reaction_rate_opts': None,
-                                 'reduce_chain': False,
-                                 'reduce_chain_level': None},
-             'output': True,
-             'integrator_kwargs': {}}
+            expected_depletion_settings
     elif codename == 'serpent':
         assert depcode_input['template_input_file_path'] == \
             str((input_path / 'tap_template.ini').resolve())
@@ -58,6 +62,48 @@ def test_read_main_input(cwd, codename, ext):
 
     assert reactor_input['timestep_units'] == 'd'
     assert reactor_input['timestep_type'] == 'stepwise'
+
+@pytest.mark.parametrize("filename", [
+    "constant_fission_yield",
+    "cutoff_fission_yield",
+    "flux_reaction_rate"])
+def test_openmc_depletion_settings(cwd, filename):
+    data_path = 'openmc_data'
+    data_path = cwd / data_path
+    main_input = str(data_path / f'{filename}_input.json')
+    out = read_main_input(main_input)
+    input_path, process_input_file, path_input_file, object_input = out
+    depcode_input, simulation_input, reactor_input = object_input
+
+    assert depcode_input['template_input_file_path'] == \
+        {'materials': str((input_path / 'tap_materials.xml').resolve()),
+         'settings': str((input_path / 'tap_settings.xml').resolve())}
+    assert depcode_input['chain_file_path'] == \
+        str((input_path / 'test_chain.xml').resolve())
+
+    modified_operator_kwargs = expected_depletion_settings['operator_kwargs'].copy()
+    if filename == 'constant_fission_yield':
+
+        operator_kwargs = {'fission_yield_opts': {'energy': 0.0253}}
+        modified_operator_kwargs.update(operator_kwargs)
+    elif filename == 'cutoff_fission_yield':
+        operator_kwargs = {'fission_yield_mode': 'cutoff',
+                           'fission_yield_opts': {'cutoff': 112.0,
+                                                  'thermal_energy': 0.0253,
+                                                  'fast_energy': 5.0e5}}
+        modified_operator_kwargs.update(operator_kwargs)
+
+    elif filename == 'flux_reaction_rate':
+        operator_kwargs = {'reaction_rate_mode': 'flux',
+                           'reaction_rate_opts': {'energies': [0.0253, 500000.0],
+                                                  'reactions': ['(n,gamma)'],
+                                                  'nuclides': ['U235', 'Pu239']}
+                           }
+        modified_operator_kwargs.update(operator_kwargs)
+
+    assert depcode_input['depletion_settings']['operator_kwargs'] == \
+            modified_operator_kwargs
+
 
 def test_get_extraction_processes(proc_test_file):
     procs = get_extraction_processes(proc_test_file)
